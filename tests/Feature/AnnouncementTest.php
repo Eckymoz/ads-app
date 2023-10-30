d789ac07b3eb1edeb29c8541a8aebfdeb36e5c26
gregory <gregory5904@hotmail.fr>
2023-10-30 11:53:28 +0100

    [ADD] Pest and add new tests

--- tests/Feature/AnnouncementTest.php
+++ tests/Feature/AnnouncementTest.php
@@ -3,25 +3,20 @@
 namespace Tests\Feature;
 
 use App\Models\User;
-use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\Storage;
-use Tests\TestCase;
 
-class AnnouncementTest extends TestCase
-{
-    use RefreshDatabase;
-    public function test_announcement_creation_with_validation()
-    {
+it('should create an announcement with validation', function () {
         Storage::fake('public');
         $user = User::factory()->create();
         $this->actingAs($user);
 
         $data = [
-            'title' => 'Example Title',
+            'title'       => 'Example Title',
             'description' => 'Example Description',
-            'budget' => 100,
-            'image' => UploadedFile::fake()->image('example.jpg'),
+            'budget'      => 100,
+            'image'       => UploadedFile::fake()->image('example.jpg'),
+            'categories' => ['Category1', 'Category2'],
         ];
 
         $response = $this->post(route('announcements.store'), $data);
@@ -29,10 +24,107 @@ class AnnouncementTest extends TestCase
         $response->assertStatus(302);
 
         $this->assertDatabaseHas('announcements', [
-            'user_id' => $user->id,
-            'title' => 'Example Title',
+            'user_id'     => $user->id,
+            'title'       => 'Example Title',
             'description' => 'Example Description',
-            'budget' => 100,
+            'budget'      => 100,
         ]);
-    }
-}
+});
+
+it('should fail if title is not a string', function () {
+    Storage::fake('public');
+    $user = User::factory()->create();
+    $this->actingAs($user);
+
+    $data = [
+        'title' => 123,
+        'description' => 'Example Description',
+        'budget' => 100,
+        'image' => UploadedFile::fake()->image('example.jpg'),
+        'categories' => ['Category1', 'Category2'],
+    ];
+
+    $response = $this->post(route('announcements.store'), $data);
+
+    $response->assertSessionHasErrors('title');
+});
+
+it('should fail if title is missing', function () {
+    Storage::fake('public');
+    $user = User::factory()->create();
+    $this->actingAs($user);
+
+    $data = [
+        // 'title' => 'Example Title',
+        'description' => 'Example Description',
+        'budget' => 100,
+        'image' => UploadedFile::fake()->image('example.jpg'),
+        'categories' => ['Category1', 'Category2'],
+    ];
+
+    $response = $this->post(route('announcements.store'), $data);
+
+    $response->assertSessionHasErrors('title');
+});
+
+it('should fail if title exceeds 255 characters', function () {
+    Storage::fake('public');
+    $user = User::factory()->create();
+    $this->actingAs($user);
+
+    $data = [
+        'title' => str_repeat('A', 256),
+        'description' => 'Example Description',
+        'budget' => 100,
+        'image' => UploadedFile::fake()->image('example.jpg'),
+        'categories' => ['Category1', 'Category2'],
+    ];
+
+    $response = $this->post(route('announcements.store'), $data);
+
+    $response->assertSessionHasErrors('title');
+});
+
+it('should fail if image is not an image', function () {
+    Storage::fake('public');
+    $user = User::factory()->create();
+    $this->actingAs($user);
+
+    $data = [
+        'title' => 'Example Title',
+        'description' => 'Example Description',
+        'budget' => 100,
+        'image' => UploadedFile::fake()->create('example.txt'), // Un fichier texte au lieu d'une image
+        'categories' => ['Category1', 'Category2'],
+    ];
+
+    $response = $this->post(route('announcements.store'), $data);
+
+    $response->assertSessionHasErrors('image');
+});
+
+it('should fail if image exceeds 2048 KB', function () {
+    Storage::fake('public');
+    $user = User::factory()->create();
+    $this->actingAs($user);
+
+    $largeImage = 'public/images/image-test.jpg';
+
+    $data = [
+        'title' => 'Example Title',
+        'description' => 'Example Description',
+        'budget' => 100,
+        'image' => $largeImage,
+        'categories' => ['Category1', 'Category2'],
+    ];
+
+    $response = $this->post(route('announcements.store'), $data);
+
+    $response->assertSessionHasErrors('image');
+});
+
+
+
+
+
+
