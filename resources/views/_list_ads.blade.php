ec51c31ba263524018fb860890d7a666b8c65c10
gregory <gregory5904@hotmail.fr>
2023-11-27 10:21:18 +0100

    Home > Ads list > Replace svg

--- resources/views/_list_ads.blade.php
+++ resources/views/_list_ads.blade.php
@@ -28,44 +28,12 @@
                                             <div
                                                 class="mt-3 list-inline list-inline-dots mb-0 text-secondary d-sm-block d-none">
                                                 <div class="list-inline-item">
-                                                    <svg xmlns="http://www.w3.org/2000/svg"
-                                                         class="icon icon-inline" width="24" height="24"
-                                                         viewBox="0 0 24 24" stroke-width="2"
-                                                         stroke="currentColor" fill="none"
-                                                         stroke-linecap="round" stroke-linejoin="round">
-                                                        <path stroke="none" d="M0 0h24v24H0z"
-                                                              fill="none"/>
-                                                        <path
-                                                            d="M8 9l5 5v7h-5v-4m0 4h-5v-7l5 -5m1 1v-6a1 1 0 0 1 1 -1h10a1 1 0 0 1 1 1v17h-8"/>
-                                                    </svg>
-                                                    {{ $announcement->company }}
+                                                    <i class="ti ti-user"></i>
+                                                    <span class="fs-5">{{ $announcement->user->name }}</span>
                                                 </div>
                                                 <div class="list-inline-item">
-                                                    <svg xmlns="http://www.w3.org/2000/svg"
-                                                         class="icon icon-inline" width="24" height="24"
-                                                         viewBox="0 0 24 24" stroke-width="2"
-                                                         stroke="currentColor" fill="none"
-                                                         stroke-linecap="round" stroke-linejoin="round">
-                                                        <path stroke="none" d="M0 0h24v24H0z"
-                                                              fill="none"/>
-                                                        <path
-                                                            d="M15 21h-9a3 3 0 0 1 -3 -3v-1h10v2a2 2 0 0 0 4 0v-14a2 2 0 1 1 2 2h-2m2 -4h-11a3 3 0 0 0 -3 3v11"/>
-                                                    </svg>
-                                                    {{ $announcement->job_type }}
-                                                </div>
-                                                <div class="list-inline-item">
-                                                    <svg xmlns="http://www.w3.org/2000/svg"
-                                                         class="icon icon-inline" width="24" height="24"
-                                                         viewBox="0 0 24 24" stroke-width="2"
-                                                         stroke="currentColor" fill="none"
-                                                         stroke-linecap="round" stroke-linejoin="round">
-                                                        <path stroke="none" d="M0 0h24v24H0z"
-                                                              fill="none"/>
-                                                        <path d="M9 11a3 3 0 1 0 6 0a3 3 0 0 0 -6 0"/>
-                                                        <path
-                                                            d="M17.657 16.657l-4.243 4.243a2 2 0 0 1 -2.827 0l-4.244 -4.243a8 8 0 1 1 11.314 0z"/>
-                                                    </svg>
-                                                    {{ $announcement->location }}
+                                                    <i class="ti ti-calendar-event"></i>
+                                                   <span class="fs-5">{{ $announcement->created_at }}</span>
                                                 </div>
                                             </div>
                                             <div
