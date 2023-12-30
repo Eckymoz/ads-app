68a0adb1f07be7d89ab2eb10ef79a816bc441a2c
gregory <gregory5904@hotmail.fr>
2023-12-30 19:01:38 +0100

    Login > Edit layout

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,20 +2,20 @@
 
 @section('content')
     <div class="page page-center">
-        <div class="container container-tight py-4">
+        <div class="container container-tight py-2">
             <div class="text-center mb-4">
                 <a href="{{ url('/') }}" class="navbar-brand navbar-brand-autodark">
-                    <img src="{{ asset('static/logo.svg') }}" width="110" height="32" alt="Tabler" class="navbar-brand-image">
+                    <img src="{{ asset('images/logo.jpeg') }}" alt="JoinMyCrew" class="avatar avatar-xl">
                 </a>
             </div>
             <div class="card card-md">
                 <div class="card-body">
-                    <h2 class="h2 text-center mb-4">Login to your account</h2>
+                    <h2 class="h2 text-center mb-4">Connectez-vous à votre compte</h2>
                     <form method="POST" action="{{ route('login') }}" autocomplete="off">
                         @csrf
                         <div class="mb-3">
-                            <label class="form-label">Email address</label>
-                            <input type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" placeholder="your@email.com">
+                            <label class="form-label">Email</label>
+                            <input type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" placeholder="Votre email">
                             @error('email')
                             <span class="invalid-feedback" role="alert">
                                 <strong>{{ $message }}</strong>
@@ -24,13 +24,13 @@
                         </div>
                         <div class="mb-2">
                             <label class="form-label">
-                                Password
+                                Mot de passe
                                 <span class="form-label-description">
-                                <a href="{{ route('password.request') }}">I forgot password</a>
+                                <a href="{{ route('password.request') }}">Mot de passe oublié ?</a>
                             </span>
                             </label>
                             <div class="input-group input-group-flat">
-                                <input type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password" placeholder="Your password">
+                                <input type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password" placeholder="Votre mot de passe">
                                 <span class="input-group-text">
                                 <a href="#" class="link-secondary" title="Show password" data-bs-toggle="tooltip">
                                     <!-- Ajoutez ici l'icône SVG de l'œil pour afficher le mot de passe -->
@@ -43,14 +43,9 @@
                             </span>
                             @enderror
                         </div>
-                        <div class="mb-2">
-                            <label class="form-check">
-                                <input type="checkbox" class="form-check-input" name="remember" {{ old('remember') ? 'checked' : '' }}>
-                                <span class="form-check-label">Remember me on this device</span>
-                            </label>
-                        </div>
+
                         <div class="form-footer">
-                            <button type="submit" class="btn btn-primary w-100">Sign in</button>
+                            <button type="submit" class="btn btn-primary w-100">Se connecter</button>
                         </div>
                     </form>
                 </div>
@@ -67,7 +62,7 @@
                 </div>
             </div>
             <div class="text-center text-secondary mt-3">
-                Don't have an account yet? <a href="{{ route('register') }}" tabindex="-1">Sign up</a>
+                Vous n'avez pas de compte ? <a href="{{ route('register') }}" tabindex="-1">Créer un compte</a>
             </div>
         </div>
     </div>
